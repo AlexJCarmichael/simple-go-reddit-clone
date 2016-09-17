c02b8eab3962dd5bbaceac9537806df60de2cde4
Dane Carmichael <carmichaeldane@gmail.com>
2016-09-16 20:09:14 -0500

    Finished up the server and routing as laid out in http://thenewstack.io/make-a-restful-json-api-go/

--- my-server.go
+++ my-server.go
@@ -3,16 +3,31 @@ package main
 
 import (
   "fmt"
-  "html"
   "log"
   "net/http"
+
+  "github.com/gorilla/mux"
 )
 
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "%q",
-    html.EscapeString(r.URL.Path))
-  })
+  router := mux.NewRouter().StrictSlash(true)
+  router.HandleFunc("/", Root)
+  router.HandleFunc("/links", LinkIndex)
+  router.HandleFunc("/links/{linkId}", LinkShow)
+  log.Fatal(http.ListenAndServe(":8080", router))
+}
 
-  log.Fatal(http.ListenAndServe(":8080", nil))
+
+func Root(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Landing page for my slim reddit")
+}
+
+func LinkIndex(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Link Index Page")
+}
+
+func LinkShow(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  linkId := params["linkId"]
+  fmt.Fprintf(w, "Link show:", linkId)
 }

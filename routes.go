143a768084a9e52485e178f8be797978d14bc18e
Dane Carmichael <carmichaeldane@gmail.com>
2016-09-16 21:30:37 -0500

    Adding a basic logger as outline in http://thenewstack.io/make-a-restful-json-api-go/

--- routes.go
+++ routes.go
@@ -16,16 +16,22 @@ type Route struct {
 type Routes []Route
 
 func NewRouter() *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(route.HandlerFunc)
-	}
 
-	return router
+  router := mux.NewRouter().StrictSlash(true)
+  for _, route := range routes {
+    var handler http.Handler
+
+    handler = route.HandlerFunc
+    handler = Logger(handler, route.Name)
+
+    router.
+      Methods(route.Method).
+      Path(route.Pattern).
+      Name(route.Name).
+      Handler(handler)
+  }
+
+  return router
 }
 
 var routes = Routes{
